a5c6b3ac7092b2cc3e666510cff2ef2685655fde
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-02-26 15:21:14 -0500

    serialize/deserialize test

--- src/test/java/us/yellosoft/thrift/HeadphonesTest.java
+++ src/test/java/us/yellosoft/thrift/HeadphonesTest.java
@@ -1,22 +1,46 @@
 package us.yellosoft.thrift;
 
+import org.apache.thrift.TSerializer;
+import org.apache.thrift.TDeserializer;
+import org.apache.thrift.TException;
+
 import org.junit.Test;
+import org.junit.BeforeClass;
 import static org.junit.Assert.assertEquals;
 
 public class HeadphonesTest {
-  @Test
-  public void constructorTest() {
-    final String brand = "Brand";
-    final int quantity = 1;
-    final double price = 250.0;
+  private static final String brand = "Brand";
+  private static final int quantity = 1;
+  private static final double price = 250.0;
 
-    final Headphones h = new Headphones();
+  private static Headphones h;
+  private static TSerializer serializer;
+  private static TDeserializer deserializer;
+
+  @BeforeClass
+  public static void setupTest() {
+    h = new Headphones();
     h.setBrand(brand);
     h.setQuantity(quantity);
     h.setPrice(price);
 
+    serializer = new TSerializer();
+    deserializer = new TDeserializer();
+  }
+
+  @Test
+  public void constructorTest() {
     assertEquals(h.getBrand(), brand);
     assertEquals(h.getQuantity(), quantity);
     assertEquals(h.getPrice(), price, 0.0);
+  }
+
+  @Test
+  public void serializationTest() throws TException {
+    final byte[] byteArray = serializer.serialize(h);
+    final Headphones h2 = new Headphones();
+    deserializer.deserialize(h2, byteArray);
+
+    assertEquals(h2, h);
   }
 }
