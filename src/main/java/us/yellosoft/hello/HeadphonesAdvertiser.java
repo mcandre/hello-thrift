4b8f01204959ca6430740334bd9aed74d0a06ca3
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-09-28 14:46:06 -0500

    pmd

--- src/main/java/us/yellosoft/hello/HeadphonesAdvertiser.java
+++ src/main/java/us/yellosoft/hello/HeadphonesAdvertiser.java
@@ -2,7 +2,15 @@ package us.yellosoft.hello;
 
 import us.yellosoft.hello.thrift.Headphones;
 
-public class HeadphonesAdvertiser {
+/** Example user of Headphones class */
+public final class HeadphonesAdvertiser {
+  /** Utility class */
+  private HeadphonesAdvertiser() {}
+
+  /** Create an advertisement for a headphone model
+      @param headphones the model to advertise
+      @return an advertisement
+   */
   public static String advertise(final Headphones headphones) {
     return "This week only, buy " + headphones.getBrand() + "\'s elite headphones 50% off, " +
       "at " + headphones.getPrice() + "!\n" +
@@ -11,6 +19,9 @@ public class HeadphonesAdvertiser {
       "Customers remark, \"" + headphones.getReview() + "\" !";
   }
 
+  /** CLI entry point
+      @param args CLI flags
+   */
   public static void main(final String[] args) {
     final Headphones headphones = new Headphones();
     headphones.setBrand("Bose");
